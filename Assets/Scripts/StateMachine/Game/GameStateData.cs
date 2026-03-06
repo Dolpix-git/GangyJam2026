79c440c2ff8762dcfdfcce0d4429671e4a760ac7
KieranVelez <kieran.velez@codewizards.co.uk>
2026-03-06 20:59:22 +0000

    Feature: First implementation of a boot strapper method for the entriy game sate.

--- Assets/Scripts/StateMachine/Game/GameStateData.cs
+++ Assets/Scripts/StateMachine/Game/GameStateData.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CardGame.StateMachine.Game
 {
     public class GameStateData : MonoBehaviour
     {
+        [Header("Prefabs")]
+        [SerializeField] public GameObject playerPrefab;
+        [SerializeField] public GameObject cardPrefab;
+
+        [Header("Runtime")]
+        public List<GameObject> Players { get; } = new();
+
         public Action<IState<GameStateData>> GoToState { get; set; }
     }
 }
--- Assets/Scripts/StateMachine/Game/States/EnterGameState.cs
+++ Assets/Scripts/StateMachine/Game/States/EnterGameState.cs
@@ -1,12 +1,18 @@
+using CardGame.Player;
 using UnityEngine;
 
 namespace CardGame.StateMachine.Game.States
 {
     public class EnterGameState : IState<GameStateData>
     {
+        private const int PlayerCount = 2;
+        private const int CardsPerDeck = 3;
+
         public void OnEnter(GameStateData ctx)
         {
-            Debug.Log("[EnterGame] Game is starting. Press SPACE to begin.");
+            Debug.Log("[EnterGame] Bootstrapping game...");
+            SpawnPlayers(ctx);
+            Debug.Log("[EnterGame] Ready. Press SPACE to begin.");
         }
 
         public void OnUpdate(GameStateData ctx)
@@ -19,7 +25,32 @@ namespace CardGame.StateMachine.Game.States
 
         public void OnExit(GameStateData ctx)
         {
-            Debug.Log("[EnterGame] Leaving enter game phase.");
+            Debug.Log("[EnterGame] Starting game.");
+        }
+
+        private void SpawnPlayers(GameStateData ctx)
+        {
+            ctx.Players.Clear();
+
+            for (var i = 0; i < PlayerCount; i++)
+            {
+                var playerObj = Object.Instantiate(ctx.playerPrefab);
+                playerObj.name = $"Player{i + 1}";
+
+                var playerData = playerObj.GetComponent<PlayerData>();
+                playerData.Initialize($"Player {i + 1}");
+
+                var deck = playerObj.GetComponent<PlayerDeck>();
+                for (var j = 0; j < CardsPerDeck; j++)
+                {
+                    var card = Object.Instantiate(ctx.cardPrefab);
+                    card.name = $"Card_P{i + 1}_{j + 1}";
+                    deck.AddCard(card);
+                }
+
+                ctx.Players.Add(playerObj);
+                Debug.Log($"[EnterGame] Spawned {playerData.PlayerName} with {CardsPerDeck} cards in deck.");
+            }
         }
     }
 }
