97d00c233c063371e839471b6e23ed453369e73b
Yash Vishwanath <yashvishwanath@yahoo.com>
2026-03-07 15:07:35 +0000

    Feature: ModelView PlayerDeck

---
+++ Assets/Scripts/UI/ModelView/Models/ModelViewDeck.cs
@@ -0,0 +1,32 @@
+using CardGame.Player;
+using CardGame.UI.ModelViewPattern;
+using UnityEngine;
+
+namespace UI.ModelView.Models
+{
+    public class ModelViewDeck: ModelViewBase<PlayerDeck>
+    {
+        [SerializeField] private PlayerDeck deck;
+
+        private void Start()
+        {
+            if (deck == null)
+            {
+                Debug.LogWarning("Deck not set in ModelViewDeck");
+                return;
+            }
+
+            SetModel(deck);
+        }
+
+        public override void SetModel(PlayerDeck model)
+        {
+            if (model != null)
+            {
+                
+            }
+            
+            base.SetModel(model);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ModelView/Views/ViewDeck.cs
@@ -0,0 +1,49 @@
+using System;
+using CardGame.Player;
+using CardGame.UI.ModelViewPattern;
+using TMPro;
+using UI.ModelView.Models;
+using UnityEngine;
+
+namespace UI.ModelView.Views
+{
+    public class ViewDeck: ViewBase<ModelViewDeck, PlayerDeck>
+    {
+        [SerializeField] private TMP_Text deckNumberCount;
+        
+        private PlayerDeck deck;
+        
+        protected override void HandleModelChanged(PlayerDeck model)
+        {
+            if (model == null)
+            {
+                return;
+            }
+
+            if (deck != null)
+            {
+                deck.OnDeckChanged -= UpdateDeckCount;
+            }
+
+            deck = model;
+            deck.OnDeckChanged += UpdateDeckCount;
+
+            UpdateDeckCount();
+        }
+
+        private void OnDestroy()
+        {
+            if (deck == null)
+            {
+                return;
+            }
+            
+            deck.OnDeckChanged += UpdateDeckCount;
+        }
+
+        private void UpdateDeckCount()
+        {
+            deckNumberCount.text = deck.Count.ToString();
+        }
+    }
+}
