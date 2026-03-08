199445132b8732d6afd1a08262c6afeb78a597fe
Yash Vishwanath <yashvishwanath@yahoo.com>
2026-03-08 06:46:13 +0000

    Feature: Ability Button Highlights

--- Assets/Scripts/Card/Components/CardMode.cs
+++ Assets/Scripts/Card/Components/CardMode.cs
@@ -1,18 +1,28 @@
+using System;
 using UnityEngine;
 
 namespace CardGame.Data
 {
     public class CardMode : MonoBehaviour
     {
-        public int SelectedAbilityIndex { get; set; } = -1;
+        public int SelectedAbilityIndex { get; private set; } = -1;
         public string TargetingData { get; set; }
 
         public bool HasSelection => SelectedAbilityIndex >= 0;
+
+        public event Action OnAbilitySelect;
+
+        public void SelectAbility(int abilityIndex)
+        {
+            SelectedAbilityIndex = abilityIndex;
+            OnAbilitySelect?.Invoke();
+        }
 
         public void Clear()
         {
             SelectedAbilityIndex = -1;
             TargetingData = null;
+            OnAbilitySelect?.Invoke();
         }
     }
 }
