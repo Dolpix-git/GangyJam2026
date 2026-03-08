ecf4fce51ef5c6254569903ec3f43783039a3d93
Yash Vishwanath <yashvishwanath@yahoo.com>
2026-03-08 03:52:23 +0000

    Feature: CardDropOnSlotEvent

--- Assets/Scripts/Drag/CardDragHandler.cs
+++ Assets/Scripts/Drag/CardDragHandler.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Events;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -15,6 +17,8 @@ public class CardDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        EventBus.Publish(new CardBeginDragEvent(this));
+        
         _originalParent = _cardVisual.parent;
         _cardVisual.SetParent(_canvas.transform);
     }
@@ -23,9 +27,39 @@ public class CardDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         _cardVisual.anchoredPosition += eventData.delta / _canvas.scaleFactor;
     }
+    
+    private CardDropSlot GetCardDropSlotUnderPointer(PointerEventData eventData)
+    {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        foreach (var result in results)
+        {
+            if (result.gameObject == gameObject)
+            {
+                continue;
+            }
+
+            var slot = result.gameObject.GetComponentInParent<CardDropSlot>();
+
+            if (slot != null)
+            {
+                return slot;
+            }
+        }
+
+        return null;
+    }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        var dropSlot = GetCardDropSlotUnderPointer(eventData);
+
+        if (dropSlot != null)
+        {
+            EventBus.Publish(new CardDropOnSlotEvent(dropSlot));
+        }
+        
         _cardVisual.SetParent(_originalParent);
         _cardVisual.anchoredPosition = Vector2.zero;
     }
